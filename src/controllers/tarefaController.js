f4c2f2a8f0928e844061e990ba9e11b2915a4427
Julia Martins <juliamartins.td2.senai@gmail.com>
2025-03-20 15:21:54 -0300

    feat: create update method

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -16,7 +16,7 @@ class TarefaController {
       if (!descricao) {
         return res.status(400).json({ erro: "Descrição é obrigatória" });
       }
-      const novaTarefa = tarefaModel.create(descricao);
+      const novaTarefa = await tarefaModel.create(descricao);
       res.status(201).json(novaTarefa);
       
     } catch (error) {
@@ -24,13 +24,29 @@ class TarefaController {
       res.status(500).json({erro: "Erro ao criar tarefa"});
     }
   };
-  update = ({ params: { id }, body: { concluida } }, res) => {
-    const tarefaAtualizada = tarefaModel.update(id, concluida);
-    if (!tarefaAtualizada) {
-      return res.status(404).json({ erro: "Tarefa não encontrada" });
+
+  update = async (req, res) => {
+    const {id} = req.params;
+    const { concluida, descricao } = req.body;
+
+    try {
+      const tarefaAtualizada = await tarefaModel.update(
+    Number(id),
+    concluida, 
+    descricao
+  );
+
+  if (!tarefaAtualizada) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+      res.json(tarefaAtualizada);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ erro: "Erro ao atualizar tarefa" });
     }
-    res.json(tarefaAtualizada);
+    
   };
+
   delete = ({ params: { id } }, res) => {
     const sucesso = tarefaModel.delete(id);
     if (!sucesso) {
